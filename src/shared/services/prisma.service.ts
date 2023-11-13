a09caf439c570d8a476e7e3d76ed4a411fdcac37
Bartlomiej Pedryc <b.pedryc@poczta.fm>
2023-11-13 14:19:57 +0100

    Revert prisma.service to version with BeforeExit hook

--- src/shared/services/prisma.service.ts
+++ src/shared/services/prisma.service.ts
@@ -1,40 +1,15 @@
-import { INestApplication, Injectable, OnModuleDestroy } from '@nestjs/common';
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleDestroy {
-  async onModuleDestroy() {
-    await this.$disconnect();
-  }
-
+export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
   }
 
   async enableShutdownHooks(app: INestApplication) {
-    const handleAppClose = async () => {
+    this.$on('beforeExit', async () => {
       await app.close();
-      await this.$disconnect();
-      process.exit(0);
-    };
-
-    process.on('SIGTERM', handleAppClose);
-    process.on('SIGINT', handleAppClose);
+    });
   }
 }
-
-// import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-// import { PrismaClient } from '@prisma/client';
-
-// @Injectable()
-// export class PrismaService extends PrismaClient implements OnModuleInit {
-//   async onModuleInit() {
-//     await this.$connect();
-//   }
-
-//   async enableShutdownHooks(app: INestApplication) {
-//     this.$on('beforeExit', async () => {
-//       await app.close();
-//     });
-//   }
-// }
